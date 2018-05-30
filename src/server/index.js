2b465f9dd4a399ea72028b424c2dc1410cee479a
Saikat Bhattacharya <saikat.bhattacharya@nationstarmail.com>
2018-05-30 17:32:04 +0530

    pm2 port - constant

--- src/server/index.js
+++ src/server/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const route = require('./router');
 
 const app = express();
-const port = process.env.PORT||5000;
+const port = 5000;//process.env.PORT||5000;
 
 app.use(cors());
 app.use(morgan('combined'));
