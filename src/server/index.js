d0a9126c195d6cdd495ab3a32f6fdd1d954dbf03
Saikat Bhattacharya <saikat.bhattacharya@nationstarmail.com>
2018-05-30 16:40:28 +0530

    bug fixed

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,13 @@
-import express from 'express';
-import cors from 'cors';
-import route from './router';
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const route = require('./router');
 
 const app = express();
 const port = process.env.PORT||5000;
 
 app.use(cors());
+app.use(morgan('combined'));
 
 route(app);
 
--- src/server/router/index.js
+++ src/server/router/index.js
@@ -1,10 +1,10 @@
-import bodyParser from 'body-parser';
-import path from 'path';
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const router = (app) => {
     app.get('/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../../../index.html'));
+        res.json('Hi there!');
     })
 }
 
-export default router;
+module.exports = router;
